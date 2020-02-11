edc5491a0eb33ab1be82a3e4f9d80ce0f6814cbb
Niloy009 <niloy.roy@northsouth.edu>
2020-02-11 19:53:04 +0600

    intialize the router

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//router files
+const bootcamp = require('./routes/bootcamps');
+
 //load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamp);
 
 const PORT = process.env.PORT || 5000;
 
